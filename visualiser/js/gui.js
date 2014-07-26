9228b6c8b33eda0742bb5cfcc127579c6e528354
Surya Mattu <surya@suryamattu.com>
2014-07-26 16:15:46 -0400

    gui stuff

---
+++ visualiser/js/gui.js
@@ -0,0 +1,11 @@
+function Params() {
+
+  this.dbName = "test";
+  this.remoteServer  = 'http://127.0.0.1:5984/test';
+  this.layout = []; //= [ 'ConnectionsRealTimes', 'Distance' , 'Connections' ];
+  this.refreshRate = 10;
+  this.hours = 0;
+  this.minutes = 3;
+  this.seconds = 0;
+
+};
