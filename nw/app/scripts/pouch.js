5c7c21933492f755f15c15938fcdb61234f3f485
Surya Mattu <surya@suryamattu.com>
2014-08-04 23:14:46 -0400

    added nw

---
+++ nw/app/scripts/pouch.js
@@ -0,0 +1,174 @@
+var PouchDB = require('PouchDB');
+var utils = require('./utils');
+var Promise = require('es6-promise').Promise;
+module.exports = function(){
+	// var syncDom = document.getElementById('sync-wrapper');
+	// var dbName = 'Nodes'
+
+  var db;
+	var remoteCouch;
+
+	function pouch(){
+
+		db = new PouchDB(utils.config.dbName);
+		remoteCouch = utils.config.remoteServer;
+
+		console.log(utils.config.remoteServer);
+		db.info(function(err, info) {
+			// db.changes({
+			// 	since: info.update_seq,
+			// 	live: true
+			// }).on('change', changeEvent);
+			if(err){
+				console.log(err)
+			};
+
+		});
+
+		if (remoteCouch) {
+			sync();
+			// init();
+		}
+	}
+
+	function init(){
+		db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+			console.log(doc.rows);
+		});
+	}
+
+  // Show the current list of todos by reading them from the database
+  function changeEvent() {
+		db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+			console.log(doc.rows);
+		});
+  }
+
+
+	// done to get individual nodes
+	pouch.getDB = function (){
+		// db.get(docId, [options], [callback])
+	};
+
+	// done to carry a query. Can pass a new Map function if needed
+	pouch.queryByTime = function(network,time, firstTime){
+		// console.log(time);
+		console.log(time +" : "+ setUTCDuration(0,0,0));
+		var opts = {//startkey:time,
+								endkey:time,
+								reduce: false,
+								descending: false};
+
+		db.query("lastTimeSeen", opts, function(err, response) {
+
+			if(err){ console.log(err); }
+			else{
+					var postData = [];
+					for (var row in response.rows){
+						// console.log(response.rows[row]);
+						postData.push(response.rows[row].value);
+
+					}
+				if(firstTime){
+
+					network('#vis',postData);
+					network.updateData(postData);
+				}else{
+					// console.log("Other Time ");
+					network.updateData(postData);
+				}
+				// console.log(postData.length);
+			}
+		});
+	};
+
+	// done to carry a query. Can pass a new Map function if needed
+	pouch.queryByTimestamp = function(network,time, firstTime){
+		// console.log(time);
+		// console.log(time +" : "+ Date.now());
+		var opts = {startkey:time,
+								endkey:parseInt(Date.now()/1000),
+								reduce: false,
+								descending: false};
+		// console.log(db);
+		db.query("timestamp", opts, function(err, response) {
+
+			if(err){ console.log(err); }
+			else{
+					var postData = [];
+					for (var row in response.rows){
+						// console.log(response.rows[row]);
+						postData.push(response.rows[row].value);
+
+					}
+				if(firstTime){
+					// console.log(	postData);
+					network('#vis',postData);
+
+					network.updateData(postData);
+				}else{
+					// conssole.log("Other Time ");
+					network.updateData(postData);
+				}
+				console.log(postData.length);
+			}
+		});
+	};
+	pouch.queryByPower = function (network){
+
+		var opts = {reduce: false,descending: true};
+		db.query("power", opts, function(err, response) {
+
+			if(err){ console.error(err); }
+			else{
+					var postData =[];
+					for (var row in response.rows){
+						// console.log(response.rows[row].key + " : " + response.rows[row].value);
+						postData.push(response.rows[row].value);
+					}
+				network('#vis',postData);
+			}
+		});
+	};
+
+	pouch.addQuery = function(network, map, opts, firstTime){
+
+		opts.reduce = opts.reduce ||  "false";
+		db.query({map:map},opts,function(err,response){
+			var postData = [];
+			for (var row in response.rows){
+				console.log(response.rows[row].key + " : " + response.rows[row].value);
+				postData.push(response.rows[row].value);
+			}
+
+			if(firstTime){
+
+				network('#vis',postData);
+			}else{
+				console.log("Other Time ");
+				network.updateData(postData);
+			}
+
+		});
+	};
+
+
+	// Sync the localDB to the remoteDB
+	sync = function() {
+    var opts = {live: true};
+    //
+		//dont think i need the replicate to
+		// db.replicate.to(remoteCouch, opts, syncError);
+		console.log(remoteCouch);
+    db.replicate.from(remoteCouch, opts, syncError);
+  }
+
+  // There was some form or error syncing
+  function syncError(err) {
+    console.log(err);
+    // syncDom.setAttribute('data-sync-state', 'error');
+  }
+
+	return pouch;
+
+};
---
+++ nw/app/scripts/utils.js
@@ -0,0 +1,50 @@
+module.exports.config = {};
+module.exports.config.dbName = "test2";
+module.exports.config.remoteServer  = 'http://127.0.0.1:5984/test2';
+module.exports.config.nodes = [];
+module.exports.config.links = [];
+module.exports.config.layouts = [ 'Distance','Connections', 'Network'];
+module.exports.config.interval = 5000;
+module.exports.config.intervalId;
+module.exports.config.hours = 0;
+module.exports.config.minutes = 3;
+module.exports.config.seconds = 0;
+
+// var node = null;
+// var link = null;
+
+module.exports.setDuration =function (numHours, numMinutes, numSeconds){
+	var ts = new Date();
+	var minutes = (ts.getMinutes()-numMinutes) >0 ? (ts.getMinutes()-numMinutes) : 0;
+	var seconds = (ts.getSeconds()-numSeconds) >0 ? (ts.getSeconds()-numSeconds) : 0;
+	var hours 	= (ts.getHours()-numHours) >0 ? (ts.getHours()-numHours) : 0;
+	var timestamp = ts.getFullYear()+"-"+(ts.getMonth()-1)+"-"+(ts.getDate()+4)+" "+(hours)+":"+ minutes	+":"+seconds;
+
+	return timestamp;
+}
+
+module.exports.setUTCDuration = function(numHours, numMinutes, numSeconds){
+
+	var ts = new Date();
+	var minutes = (ts.getUTCMinutes()-numMinutes) >0 ? (ts.getUTCMinutes()-numMinutes) : 0;
+	var seconds = (ts.getUTCSeconds()-numSeconds) >0 ? (ts.getUTCSeconds()-numSeconds) : 0;
+	var hours 	= (ts.getUTCHours()-numHours) >0 ? (ts.getUTCHours()-numHours) : 0;
+	var timestamp = ts.getFullYear()+"-"+(ts.getUTCMonth()+1)+"-"+(ts.getUTCDate())+" 0"+(hours)+":"+ minutes	+":"+seconds+"";
+	// console.log(":"+timestamp+":");
+	return timestamp;
+}
+
+module.exports.getTimeStamp = function(numHours, numMinutes, numSeconds){
+
+	var ts = new Date();
+	var minutes = (ts.getUTCMinutes()-numMinutes) >0 ? (ts.getUTCMinutes()-numMinutes) : 0;
+	var seconds = (ts.getUTCSeconds()-numSeconds) >0 ? (ts.getUTCSeconds()-numSeconds) : 0;
+	var hours 	= (ts.getUTCHours()-numHours) >0 ? (ts.getUTCHours()-numHours) : 0;
+	// var timestamp = ts.getFullYear()+"-"+(ts.getUTCMonth()+1)+"-"+(ts.getUTCDate())+" 0"+(hours)+":"+ minutes	+":"+seconds+"";
+	// console.log(":"+timestamp+":");
+
+	var millis = (60*60*numHours + 60*numMinutes + numSeconds) * 1000;
+
+	console.log(parseInt((Date.now() - millis)/1000));
+	return parseInt((Date.now() - millis)/1000);
+}
